9343d0f9340c3da1f1ff33d732278f8fa0f82d93
Rishabh Jain <jrishabh98@gmail.com>
2018-10-06 17:47:27 +0530

    Done

--- ccOCT18BHMAPPY.cpp
+++ ccOCT18BHMAPPY.cpp
@@ -1,46 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef unsigned long long int ull;
-struct st{
+typedef long long int ull;
+struct st
+{
     ull a;
     ull b;
     ull p;
 };
-bool comp (st c, st d)
+bool comp(st c, st d)
 {
-    return(c.p<d.p);
+    return (c.p < d.p);
 }
-int main ()
+int main()
 {
-    ull n,m,temp;
-    cin>>n>>m;
+    int n;
+    ull m;
+    cin >> n >> m;
     st s[n];
-    
-    for (ull i =0; i<n;i++)
-        cin>>s[i].a;
-    for (ull i =0; i<n;i++)
-        {cin>>s[i].b;
-         s[i].p = s[i].a*s[i].b;
-        }
-    sort(s,s+n,comp);
-    
-    for (ull i =0; i<n;i++)
-        cout<<s[i].p<<" ";
-    ull i= n-1;
-    while (m!=0 && i>=1)
-    {   temp = m;
-        m = m - ceil((s[i].p-s[0].p)/(float)(s[i].b));
-        if(m<0)
-            m=0;
-        if(m!=0)
-            s[i].p = s[i].p - ceil((s[i].p-s[0].p)/(float)(s[i].b))*(s[i].b);
-        else
-            s[i].p = s[i].p - temp*(s[i].b);
-        if(s[i].p<0)
-            s[i].p=0;
-        i--;
+
+    for (int i = 0; i < n; i++)
+        cin >> s[i].a;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i].b;
+        s[i].p = s[i].a * s[i].b;
     }
     sort(s,s+n,comp);
-    cout<<s[n-1].p<<"\n";
+    if (m == 0)
+    {
+        cout << s[n - 1].p << "\n";
+        return 0;
+    }
+    ull low = 0, high = 1e18, mid = 0, ans = 0, sum = 0;
+    ull mx =0;
+    while (low <= high)
+    {
+        mx=0;
+        mid = (high + low) / 2;
+        sum = 0;
+        // cout<<mid<<"\n";
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i].p >= mid)
+            {
+                sum += ceil((double)(s[i].p - mid) / (double)(s[i].b));
+                mx = max(mx,s[i].p - (ull)(ceil((double)(s[i].p-mid)/(double)(s[i].b)))*s[i].b);
+            }
+        }
+       
+        if (sum > m)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            ans = mx;
+            high = mid - 1;
+        }
+        
+    }
+    
+    cout << ans << "\n";
+    
     return 0;
 }
