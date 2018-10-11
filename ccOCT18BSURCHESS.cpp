cbda6d7ea64f5522ebdc4e7dfafd00510e5f9411
Rishabh Jain <jrishabh98@gmail.com>
2018-10-11 14:47:06 +0530

    Done

--- ccOCT18BSURCHESS.cpp
+++ ccOCT18BSURCHESS.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-int store[2][201][201][201];
-int precalx[2][201][201][201];
-int precaly[2][201][201][201];
-int counter[40001];
+int store[2][2][200][200] = {0};
+int precalx[2][2][200][200] = {0};
+int precaly[2][2][200][200] = {0};
+
 int main ()
 {
     int n,m;
@@ -11,91 +11,80 @@ int main ()
     int tempp;
     char x[m];
     int ds = min(m,n);
-    store[2][(ds)+1][n+1][m+1]={0};
-    precalx[2][ds+1][n+1][m+1] = {0};
-    precaly[2][ds+1][n+1][m+1] = {0};
     for(int i=0;i<n;i++)
-    {   //cin.get();
+    {   
         cin>>x;
         for(int j=0;j<m;j++)
         {
-            //cin>>x[j];
+            
             tempp = x[j] - (int)('0');
-            store[0][1][i+1][j+1]=abs(tempp-0);
-            store[1][1][i+1][j+1]=abs(tempp-1);
+            store[0][0][i][j]=abs(tempp-0);
+            store[1][0][i][j]=abs(tempp-1);
+            store[0][1][i][j]=store[0][0][i][j];
+            store[1][1][i][j]=store[1][0][i][j];
+            precalx[0][0][i][j]=store[0][0][i][j];
+            precalx[0][1][i][j]=precalx[0][0][i][j];
+            precalx[1][0][i][j]=store[1][0][i][j];
+            precalx[1][1][i][j]=precalx[1][0][i][j];
+            precaly[0][0][i][j]=store[0][0][i][j];
+            precaly[0][1][i][j]=precaly[0][0][i][j];
+            precaly[1][0][i][j]=store[1][0][i][j];
+            precaly[1][1][i][j]=precaly[1][0][i][j];
             
         }
     }
     int q;
     cin>>q;
     int c[q];
-    int max_size = ds*ds;
-    counter[max_size+1]={1};
+    
     for(int i=0;i<q;i++)
     {
         cin>>c[i];
     }
-    for(int i=0;i<=1;i++)
-    {
-        for(int j=2;j<=m;j++)
-            {
-                //int p = abs(i-1);
-                precaly[i][1][1][j]=store[i][1][1][j];
-            }
-    }
-    for(int i=0;i<=1;i++)
-    {
-        for(int j=2;j<=n;j++)
-            {
-                //int p = abs(i-1);
-                precalx[i][1][j][1]=store[i][1][j][1];
-            }
-    }
-    for (int i=0;i<=1;i++)
-    {
-        for(int d=2;d<=ds;d++)
-        {
-            for(int x =1; x<=(n-d+1);x++)
-            {
-                for(int y=1; y<=(m-d+1);y++)
-                {   
-                    int ld = x+d-1;
-                    int rd = y+d-1;
-                    int p = abs(i-1);
-                    precalx[i][d][ld][rd] = (store[i][1][ld][rd] + precalx[p][d-1][ld][rd-1]);
-                    precaly[i][d][ld][rd] = (store[i][1][ld][rd] + precaly[p][d-1][ld-1][rd]);
-                    store[i][d][x][y] = store[i][d-1][x][y] + (precalx[i][d][ld][rd] + precaly[i][d][ld][rd] - store[i][1][ld][rd]);
-                    int temp = store[i][d][x][y];
-                    counter[temp] = max(counter[temp],d);
-                }
-            }
-        }
-    }
-    // int mx=0;
-    // for(int i=0;i<=max_size;i++)
-    // {
-    //     mx = max(mx,counter[i]);
-    // }
-    // int i = max_size;
-    // while(counter[i]==1 && i>=0)
-    //     i--;
-    // if(i!=-1)
-    // {
-    //     for(int j = i+1; j<=max_size; j++)
-    //     {
-    //         counter[j]=mx;
-    //     }
-    // }
+
+    int counter[(ds*ds)+1]={1};
+   
+    int temp,ld,rd,p;
     
+    for(int d=2;d<=ds;d++)
+    {   
+          
+            for(int x =0; x<(n-d+1);x++)
+            {
+                for(int y=0; y<(m-d+1);y++)
+                {   
+                    for(int i=0;i<=1;i++)
+                    { 
+                    ld = x+d-1;
+                    rd = y+d-1;
+                    p = abs(i-1);
+                    precalx[i][1][ld][rd] = (store[i][0][ld][rd] + precalx[p][0][ld][rd-1]);
+                    precaly[i][1][ld][rd] = (store[i][0][ld][rd] + precaly[p][0][ld-1][rd]);
+                    store[i][1][x][y] = store[i][1][x][y] + (precalx[i][1][ld][rd] + precaly[i][1][ld][rd] - store[i][0][ld][rd]);
+                    
+                    temp = store[i][1][x][y];
+                    counter[temp] = max(counter[temp],d);
+                    precalx[p][0][ld][rd-1] = precalx[p][1][ld][rd-1];
+                    precaly[p][0][ld-1][rd] = precaly[p][1][ld-1][rd];
+                    }
+                }
+            
+            }        
+    }
+
+     for (int i=1;i<=(ds*ds);i++)
+        {counter[i] = max(counter[i],counter[i-1]);
+         //cout<<counter[i]<<" ";
+        }
+    //cout<<"\n";
     for(int i=0;i<q;i++)
     {
-        if(c[i]> max_size)
+        if(c[i]> (ds*ds))
             cout<<ds<<"\n";
         else
             cout<<counter[c[i]]<<"\n";
     }
 
-    for (int i=0;i<=max_size;i++)
-        cout<<counter[i]<<" ";
+   
     return 0;
 }
