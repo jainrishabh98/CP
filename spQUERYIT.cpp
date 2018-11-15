2ea3b6543516694e83bcb564bb343e8d9dd73b8c
Rishabh Jain <jrishabh98@gmail.com>
2018-11-16 01:59:32 +0530

    Changed Logic

--- spQUERYIT.cpp
+++ spQUERYIT.cpp
@@ -1,44 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector <int> segtree;
+int INF = INT_MAX;
+struct st{
+    int z_index = -INF;
+    int o_index = INF;
+    int first = 0;
+    int second = 0;
+};
+vector <st> segtree;
 vector <int> arr;
 int l,r;
-int INF = INT_MAX;
+
 void build (int low, int high, int pos)
 {
     if(low == high)
     {
-        segtree[pos] = arr[low];
-        // cout<<segtree[pos];
+        if(arr[low]==0)
+            {segtree[pos].first++;
+             segtree[pos].z_index == low;
+            }
+        else
+            {segtree[pos].second++;
+             segtree[pos].o_index == low;
+            }
         return ;
     }
     int mid = (low+high)/2;
     build (low, mid, 2*pos + 1);
     build (mid+1, high, 2*pos + 2);
-    if (low==mid && mid+1 == high)
-    {
-        if(segtree[2*pos + 2] >= segtree[2*pos + 1] )
-            segtree[pos] = 2;
-        else
-            segtree[pos] = 1;
-    }
-    else if (mid+1 == high)
-    {   int t = 2*pos + 1;
-        if(segtree[t+1] >= segtree[2*t + 2])
-            segtree[pos] = segtree[t] + 1;
-        else 
-            segtree[pos] = segtree[t];
-    }
-    else
-    {   int c=0;
-        int t = 2*pos + 1;
-        if(segtree[t] ==2 &&  segtree[t+1] ==2 )
-        {
-            if(segtree[2*t + 2] ==1 && segtree[2*(t+1) + 1]==0)
-                c=1;
-        }
-        segtree[pos] = segtree[t] + segtree[t+1] - c;
-    }
+    int t = 2*pos + 1;
+    segtree[pos].first = segtree[t].first + segtree[t+1].first ;
+    segtree[pos].second = segtree[t].second + segtree[t+1].second ;
+    segtree[pos].z_index = max(segtree[t].z_index,segtree[t+1].z_index);
+    segtree[pos].o_index = min(segtree[t].o_index,segtree[t+1].o_index);   
 }
 void update(int low, int high, int pos)
 {
@@ -46,40 +40,44 @@ void update(int low, int high, int pos)
         return ;
     if (low == high)
     {
-        segtree[pos]= segtree[pos]^1;
-        // cout<<segtree[pos];
+        arr[low]= arr[low]^1;
+        if(arr[low]==0)
+            {segtree[pos].first++;
+            segtree[pos].z_index == low;
+            }
+        else
+            {segtree[pos].second++;
+             segtree[pos].o_index == low;
+            }
         return ;
     }
     int mid = (low + high)/2;
     update(low,mid,2*pos + 1);
     update(mid+1,high,2*pos + 2);
-    if (low==mid && mid+1 == high)
-    {
-        if(segtree[2*pos + 2] >= segtree[2*pos + 1])
-            segtree[pos] = 2;
-        else
-            segtree[pos] = 1;
-    }
-    else if (mid+1 == high)
-    {   int t = 2*pos + 1;
-        if(segtree[t+1] >= segtree[2*t + 2])
-            segtree[pos] = segtree[t] + 1;
-        else 
-            segtree[pos] = segtree[t];
-    }
-    else
-    {   int c=0;
-        int t = 2*pos + 1;
-        if(segtree[t] ==2 &&  segtree[t+1] ==2 )
-        {
-            if(segtree[2*t + 2] ==1 && segtree[2*(t+1) + 1]==0)
-                c=1;
-        }
-        segtree[pos] = segtree[t] + segtree[t+1] - c;
-    }
-    
+    int t = 2*pos + 1;
+    segtree[pos].first = segtree[t].first + segtree[t+1].first ;
+    segtree[pos].second = segtree[t].second + segtree[t+1].second ;
+    segtree[pos].z_index = max(segtree[t].z_index,segtree[t+1].z_index);
+    segtree[pos].o_index = min(segtree[t].o_index,segtree[t+1].o_index);
+  
 }
-
+st query(int low, int high, int pos, int qlow, int qhigh)
+{
+    if(qlow<= low && qhigh>= high) // total overlap
+        return segtree[pos];
+    if(qlow > high || qhigh < low) // no overlap
+        {
+            st te;return te;
+        }
+    int mid = (low+high)/2;
+    st ans1 = query(low,mid,2*pos + 1,qlow,qhigh);
+    st ans2 = query(mid+1, high, 2*pos + 2,qlow,qhigh);
+    ans1.first += ans2.first;
+    ans1.second += ans2.second;
+    ans1.z_index = max(ans1.z_index,ans2.z_index);
+    ans1.o_index = min(ans1.o_index,ans2.o_index);
+    return ans1;
+}
 int main ()
 {
     int n,q;
@@ -89,23 +87,33 @@ int main ()
     arr.assign(n,0);
     int temp = (int)(ceil(log2(n)));
     int max_size = 2*(int)pow(2,temp) -1;
-    segtree.assign(max_size,0);
+    segtree.resize(max_size);
     for (int i=0; i<str.length(); i++)
         arr[i] = str[i] - '0';
-        
-    build(0,n-1,0);    
+    
+    
+    build(0,n-1,0);
+    
+    // qlow = 0;
+    // qhigh = n-1;
+    // pair<int,int> ans = query(0,n-1,0);  
+    int  final_ans;
     while(q--)
     {
         int x;
         cin>>x;
         if(x==1)
-            cout<<segtree[0]<<"\n";
+            {   int temp1 = (segtree[0].first + query(0,n-1,0,segtree[0].z_index+1,n-1).second);
+                int temp2 = (segtree[0].second + query(0,n-1,0,0,segtree[0].o_index-1).first);
+                final_ans = max(temp1,temp2);
+                cout<<segtree[0].z_index<<" "<<segtree[0].o_index<<" "<<temp1<<" "<<temp2<<" "<<"\n";
+                cout<<final_ans<<"\n";
+            }
         else
         {
             cin>>l>>r;
             l--;r--;
-            update(0,n-1,0);
-            
+            update(0,n-1,0);   
         }
     }
     
