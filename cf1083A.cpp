0eb104f3216a8202aeec363b7e111a00bed1609e
Rishabh Jain <jrishabh98@gmail.com>
2018-12-18 03:31:29 +0530

    To be contd

--- cf1083A.cpp
+++ cf1083A.cpp
@@ -8,52 +8,82 @@ void edge(ll a, ll b, ll wt)
     g[a].push_back(make_pair(b, wt));
     g[b].push_back(make_pair(a, wt));
 }
+ll ans = 0;
+ll fn = 0;
+ll prnt[300001];
+ll dp[300001];
 ll w[300001];
 ll mx = INT_MIN;
-ll my_dfs(ll u)
+void my_dfs(ll u)
 {
     v[u] = true;
-    ll m1=0,m2=0;
+    ans += w[u];
+    fn = max(fn,ans);
+    // ll m1=0,m2=0;
     // cout << u << " ";
        
-    for (vector<pair<ll, ll> >::iterator j = g[u].begin(); j != g[u].end(); j++)
-    {
-        if (!v[j->first] )
-        {   ll temp = my_dfs(j->first);
-            temp-= j->second;
+    // for (vector<pair<ll, ll> >::iterator j = g[u].begin(); j != g[u].end(); j++)
+    // {
+    //     if (!v[j->first] )
+    //     {   ll temp = my_dfs(j->first);
+    //         temp-= j->second;
 
-                if(temp>=m1)
-                {
-                    m2=m1;
-                    m1=temp;
-                }
-                else if(temp>=m2)
-                {
-                    m2=temp;
-                }
+    //             if(temp>=m1)
+    //             {
+    //                 m2=m1;
+    //                 m1=temp;
+    //             }
+    //             else if(temp>=m2)
+    //             {
+    //                 m2=temp;
+    //             }
             
+    //     }
+    // }
+    // mx=max(mx,m1+m2+w[u]);
+    // return m1+w[u];
+    for (vector<pair<ll, ll> >::iterator j = g[u].begin(); j != g[u].end(); j++)
+    {   
+        if (!v[j->first] && w[j->first] >= j->second)
+        {   
+            prnt[j->first] = prnt[u];
+            ans -= j->second;
+            my_dfs(j->first);
         }
+        
+        // else if (v[j->first] && dp[prnt[j->first]]!=-1 && dp[prnt[j->first]] >= j->second)
+        //     {ans += dp[j->first]; ans -= j->second;}
+        
     }
-    mx=max(mx,m1+m2+w[u]);
-    return m1+w[u];
 }
 
 int main()
 {
     int n;
     cin >> n;
+    memset(dp,-1,sizeof(dp));
     v.assign(n, false);
     g.assign(n, vector<pair<ll, ll> >());
     // int index = -1,mx=-1;
     for (int i = 0; i < n; i++)
-        cin >> w[i]; 
+        {cin >> w[i]; prnt[i]=i;}
     ll a, b, wt;
     for (int i = 1; i < n; i++)
     {
         cin >> a >> b >> wt;
         edge(a-1, b-1, wt);
     }
-    my_dfs(0);
-    cout<<mx<<"\n";
+    // my_dfs(0);
+    // cout<<mx<<"\n";
+    for(int i=0;i<n;i++)
+    { 
+        v.assign(n, false);
+        // if(!v[i])
+            my_dfs(i);
+        // dp[i] = ans;
+        // fn = max(fn,dp[i]);
+        ans=0;            
+    }
+    cout<<fn<<"\n";
     return 0;
 }
