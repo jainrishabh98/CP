3cb15a176d94c0edffd5136fe2fb08264e12cc00
jainrishabh98 <jrishabh98@gmail.com>
2018-12-23 18:05:53 +0530

    Done

--- spCANDY3.cpp
+++ spCANDY3.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+// range not given -> hence sum of all elements can exceed 1e18 also. hence take mod at every element bcuz of mod properties
 int main ()
 {   long long int t;
     cin>>t;
     while(t--)
     {
-        unsigned long long int n,x,sum=0,i;
+        unsigned long long int n,x,ans=0,i;
         cin>>n;
         for(i=0;i<n;i++)
-           { cin>>x; sum += x;}
-           
-        if (sum%n == 0)
+           { cin>>x; ans = (ans + x)%n;}
+
+        if (ans == 0)
             cout<<"YES\n";
         else
             cout<<"NO\n";
