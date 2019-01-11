838241b54441d53a1ef199c7e03ee9a3f46bcc30
jainrishabh98 <jrishabh98@gmail.com>
2019-01-12 00:23:18 +0530

    Done

--- cf264B.cpp
+++ cf264B.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector <int> v[100005];
+vector<int> v[100005];
+int dp[100005]={0};
+int d[100005]={0};
 int main()
 {
     int n;
@@ -9,30 +11,33 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        if(a[i]==1) continue;
-        for (int k = 2*a[i]; k < 100005; k += a[i])
-        {
-            v[k].push_back(a[i]);
-        }
     }
-    int dp[100005] = {0};
-    int d[100005] = {0};
-    int mx=0;dp[1]=1;
     for (int i = 2; i < 100005; i++)
-    {   mx=0;
-        for(auto k:v[i])
+    {
+        for (int k = 2 * i; k < 100005; k += i)
         {
-            mx = max(mx,dp[k]);
-        }
-        dp[i]=mx+1;
-        for(auto k:v[i])
-        {
-            d[k] = max(d[k],dp[i]);
+            v[k].push_back(i);
         }
     }
-    int ans=0;
-    for(int i=0;i<n;i++) {cout<<dp[a[i]]<<" "<<d[a[i]]<<"\n";ans = max(ans,dp[a[i]]);}
-    cout<<ans<<"\n";
+    int mx = 0;
+    for (int i = 0; i < n; i++)
+        d[a[i]]=1;
+    dp[1] = 1;int ans = 0;
+    for (int i = 0; i < n; i++)
+    {   
+        mx = 0;
+        for (auto k : v[a[i]])
+        {
+            mx = max(mx, d[k]);
+        }
+        dp[a[i]] = mx + 1;
+        for (auto k : v[a[i]])
+        {
+            d[k] = dp[a[i]];
+        }
+        // cout << dp[a[i]] << " ";
+        ans = max(ans, dp[a[i]]);
+    }
+    cout << ans << "\n";
     return 0;
-
 }
