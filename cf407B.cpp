feaefdf0c9b1e51bb946501622a2feccf20343b8
jainrishabh98 <jrishabh98@gmail.com>
2019-01-15 15:07:56 +0530

    Done

---
+++ cf407B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll n, m = 1e9 + 7;
+    cin >> n;
+    ll p[n + 1];
+    for (int i = 1; i <= n; i++)
+        cin >> p[i];
+    ll cnt[n + 1]={0};
+    ll dp[n + 1][n + 1] = {0};
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == j)
+                continue;
+            else if (j > i)
+            {
+                if (p[j] == i)
+                    dp[j][i] = 1;
+            }
+            else
+            {
+                ll temp = 0;
+                if ((cnt[i - 1] + 1) % 2 == 0)
+                {
+                    temp = 1;
+                    cnt[i - 1]++;
+                }
+                else
+                {
+                    temp = 1 + 1 + dp[p[i - 1]][i - 1];
+                    cnt[i - 1] += 2;
+                }
+
+                dp[j][i] = (dp[j][i - 1] + temp)%m;
+            }
+        }
+    }
+    cout<<(dp[1][n]+1+dp[p[n]][n]+1)%m<<"\n";
+    return 0;
+}
