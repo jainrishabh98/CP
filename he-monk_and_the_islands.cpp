f4da4c0df72d1d0cf55eafb5c9ef6a9b573c7449
jainrishabh98 <jrishabh98@gmail.com>
2019-01-25 19:08:55 +0530

    Done

--- he-monk_and_the_islands.cpp
+++ he-monk_and_the_islands.cpp
@@ -24,10 +24,23 @@ void bfs(int u)
     }
 }
 int main ()
-{
-    int n;
-    cin>>n;
-    int a[n];
-    for(int i=0;i<n;i++)    cin>>a[i];
+{   ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n>>m;
+        int x,y;
+        memset(vis,0,sizeof(vis));
+        for(int i=1;i<=m;i++)
+        {
+            cin>>x>>y;
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+        bfs(1);
+        cout<<vis[n]-1<<"\n";
+    }
 
 }
