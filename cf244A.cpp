b7782a9a653dc2680a96190f133125b6ef93826a
jainrishabh98 <jrishabh98@gmail.com>
2019-02-02 15:33:17 +0530

    Done

--- cf244A.cpp
+++ cf244A.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 int main ()
 {
-    
+    int n,k;
+    cin>>n>>k;
+    int a[k+1];
+    int vis[1000]={0};
+    for(int i=1;i<=k;i++)
+    {
+        cin>>a[i];
+        vis[a[i]]=1;
+    }   
+    int j=1;
+    for(int i=1;i<=k;i++)
+    {
+        cout<<a[i]<<" ";
+        int cnt = 1;
+        while(cnt!=n)
+        {
+            if(vis[j]==0)
+            {
+                vis[j]=1;
+                cnt++;
+                cout<<j<<" ";
+            }
+            j++;
+        }
+        cout<<"\n";
+    }
 }
