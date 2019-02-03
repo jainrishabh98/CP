c51ef1ea115ed29ae816733ea66028e1c4559a59
jainrishabh98 <jrishabh98@gmail.com>
2019-02-03 14:18:50 +0530

    To be done

---
+++ cf128C.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+ll mod = 1e9 + 7;
+int main ()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    ll dp[2][k];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            for(int p=1;p<=k;p++)
+            {
+                if(i==1 || j==1)
+                {
+                    // store 1
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
+}
