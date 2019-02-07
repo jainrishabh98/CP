7249bd2843ab4e7721eab3e83173c948651688e9
jainrishabh98 <jrishabh98@gmail.com>
2019-02-07 22:11:24 +0530

    Done

--- cf1110A.cpp
+++ cf1110A.cpp
@@ -5,6 +5,21 @@ int main ()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n;
-    cin>>n;
+    int b,k;
+    cin>>b>>k;
+    if(b%2==0) b=0;
+    else b=1;
+    int ans=0;
+    int x;
+    for(int i=0;i<k-1;i++)
+    {
+        cin>>x;
+        (ans += (x*b))%=2;
+    }
+    cin>>x;
+    (ans += x)%=2;
+    if(ans==0)
+        cout<<"even";
+    else
+        cout<<"odd";
 }
