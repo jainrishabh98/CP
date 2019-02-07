fde8a3302f2be3c03517ed749e6d3096538a928d
jainrishabh98 <jrishabh98@gmail.com>
2019-02-07 12:22:50 +0530

    Done

--- cf388A.cpp
+++ cf388A.cpp
@@ -5,20 +5,52 @@ int main ()
     int n;
     cin>>n;
     set <int> a;
+    vector <int> v;
     int x;
-    // int cnt[101]={0};
+    int cnt[101]={0};
     for(int i=0;i<n;i++)
     {   cin>>x;
-        a.insert(x);
+        cnt[x]++;
     }
     int ans = 0;
-    while(!a.empty())
-    {   auto i = a.end();i--;
-        while(i!=a.begin())
-        {   
-            
-            
+    while(*(max_element(cnt,cnt+101))!=0)
+    {
+    int num=0;
+    for(int i=0;i<=100;i++)
+    {
+        if(cnt[i]==0)
+        {
+            continue;
         }
+        else
+        {   
+            v.push_back(i);
+            cnt[i]--;
+            num++;
+        }
+    }
+    int mx = v[num-1];
+    if(*(max_element(cnt,cnt+101))!=0 && num<=mx)
+    {   int c = 0,tem=0,q;
+        for(auto j:v)
+        {   
+            if(cnt[j]!=0 && num<=mx && j!=0)
+            {   tem = 0;
+                for(q=cnt[j];q>max(0,cnt[j]-j+c);q--)
+                {
+                    tem++;
+                    num++;
+                    if(num==mx+1)
+                        {q--;break;}
+                }
+                c += tem;
+                cnt[j] = q;
+            }
+            c++;
+        }
+    }
+    ans++;
+    v.clear();
     }
     cout<<ans;
 }
