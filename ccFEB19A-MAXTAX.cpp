a6f049b6c821ed87b5fde2ba4deb6fffc28d8960
jainrishabh98 <jrishabh98@gmail.com>
2019-02-10 19:02:15 +0530

    To be done

--- ccFEB19A-MAXTAX.cpp
+++ ccFEB19A-MAXTAX.cpp
@@ -1,38 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long int ll;
+ll dp[100001][202];
+ll anss[100001][202];
 ll m = 1e9 + 21;
-ll cnt = 0;
-vector<bool> v;
-vector<vector<int>> g;
-vector<vector<int>> h;
+ll n, e, k, ans, an, cnt;
+ll t, a, c;
+vector<bool> v, rv, vf;
+vector<vector<int>> g, rg, h;
+vector<set<int>> gg;
 vector<int> b;
-void my_dfs(int u)
+int leader[100001];
+stack<int> st;
+void dfs(int u)
 {
-    h[cnt].push_back(b[u]);
     v[u] = true;
-    // cout<<u<<" ";
     for (auto j = g[u].begin(); j != g[u].end(); j++)
     {
         if (!v[*j])
+        {
+            dfs(*j);
+        }
+    }
+    st.push(u);
+}
+void my_dfs(int u)
+{
+    h[cnt].push_back(b[u]);
+    leader[u] = cnt;
+    rv[u] = true;
+    for (auto j = rg[u].begin(); j != rg[u].end(); j++)
+    {
+        if (!rv[*j])
         {
             my_dfs(*j);
         }
     }
 }
-
-int main()
+void dffs(int u)
 {
-    int t;
+    vf[u] = true;
+    for (auto j : gg[u])
+    {
+        if (!vf[j])
+        {
+            dffs(j);
+        }
+        ll l = h[u].size();
+        for (int f = 0; f <= k; f++)
+        {
+            if (f >= l)
+            {
+                dp[u][f] = max(dp[u][f], anss[j][k - f]);
+            }
+            else
+            {
+                c = h[u][f];
+                dp[u][f] = max(dp[u][f], c * (l - f) + anss[j][k - f]);
+            }
+            if (f == 0)
+                anss[u][f] = dp[u][f];
+            else
+                anss[u][f] = max(dp[u][f], anss[u][f - 1]);
+        }
+    }
+}
+
+void initialise()
+{
+    v.assign(n, false);
+    rv.assign(n, false);
+    g.assign(n, vector<int>());
+    rg.assign(n, vector<int>());
+    b.assign(n, 0);
+    memset(dp, 0, sizeof(dp));
+    memset(anss, 0, sizeof(anss));
+}
+void clear()
+{
+    h.clear();
+    g.clear();
+    gg.clear();
+    rg.clear();
+    v.clear();
+    vf.clear();
+    rv.clear();
+    b.clear();
+}
+int main()
+{   
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cin >> t;
     while (t--)
     {
-        ll n, e, k;
         cin >> n >> e >> k;
-        v.assign(n, false);
-        g.assign(n, vector<int>());
-        int a, c;
-        b.assign(n, 0);
+        int mn[e][2];
+        initialise();
         cnt = 0;
         for (int i = 0; i < n; i++)
             cin >> b[i];
@@ -41,35 +105,76 @@ int main()
             cin >> a >> c;
             a--;
             c--;
+            mn[i][0] = a;
+            mn[i][1] = c;
             g[a].push_back(c);
+            rg[c].push_back(a);
         }
         for (int i = 0; i < n; i++)
         {
             if (!v[i])
+                dfs(i);
+        }
+        while (!st.empty())
+        {
+            int x = st.top();
+            st.pop();
+            if (!rv[x])
             {
                 h.push_back(vector<int>());
-                my_dfs(i);
+                my_dfs(x);
                 cnt++;
             }
         }
         for (int i = 0; i < cnt; i++)
         {
             sort(h[i].begin(), h[i].end());
+            // for(auto j:h[i])
+            //     cout<<j<<" ";
+            // cout<<"\n";
         }
-        ll ans = 0, an = 0;
+        gg.assign(cnt, set<int>());
+        vf.assign(cnt,false);
+        for (int i = 0; i < e; i++)
+        {
+            int l1 = leader[mn[i][0]];
+            int l2 = leader[mn[i][1]];
+            if (l1 != l2)
+            {
+                gg[l1].insert(l2);
+            }
+        }
         for (int i = 0; i < cnt; i++)
         {
-            an = 0;
             ll l = h[i].size();
-            for (int j = 0; j < min(l, k + 2); j++)
+            for (int j = 0; j <= k; j++)
             {
-                ll c = h[i][j];
-                an = max(an, (ll)c * (l - j));
+                if (j >= l)
+                {
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    c = h[i][j];
+                    dp[i][j] = (c * (l - j)) % m;
+                }
+                if (j == 0)
+                    anss[i][j] = dp[i][j];
+                else
+                    anss[i][j] = max(dp[i][j], anss[i][j - 1]);
             }
-            ans = max(ans, an);
-            h[i].clear();
         }
-        cout << ans % m << "\n";
-        
+        for (int i = 0; i < cnt; i++)
+        {   
+            if(!vf[i])
+                dffs(i);
+        }
+        ans = 0;
+        for(int i=0;i<cnt;i++)
+        {
+            ans = max(ans,anss[i][k]);
+        }
+        cout<<ans<<"\n";
+        clear();
     }
 }
