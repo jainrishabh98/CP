96cfe49a28698b626bf89c6d1db3c2adb77ba564
jainrishabh98 <jrishabh98@gmail.com>
2019-03-19 15:20:13 +0530

    Done

--- cf400D.cpp
+++ cf400D.cpp
@@ -3,43 +3,31 @@ using namespace std;
 typedef pair<int,int> ipair;
 vector <ipair> g[100005];
 int d[505][505];
-int dist[100005];
 bool vis[100005];
 int mp[100005];
 int c[505],s[505];
-int INF = INT_MAX;
-void shortestPath (int src)
+void my_dfs(int u)
 {
-    priority_queue <ipair, vector <ipair> , greater <ipair> > pq;
-    dist[src]=0;
-    pq.push(make_pair(0,src));
-    while(!pq.empty())
+    vis[u] = true;
+    for (auto j:g[u])
     {
-        int u = pq.top().second;
-        pq.pop();
-        if(vis[u]) continue;
-        vis[u] = true;
-        for (auto i : g[u])
-        {
-            int v = i.first;
-            int weight = i.second;
-            if(dist[v] > dist[u] + weight)
-            {
-                dist[v] = dist[u] + weight;
-                pq.push(make_pair(dist[v],v));
-            }
+        if(!vis[j.first] && j.second==0)
+        {  
+            my_dfs(j.first);
         }
     }
 }
 int main ()
-{
+{   
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int n,m,k;
     cin>>n>>m>>k;
     for(int i=0;i<k;i++)
     {
         for(int j=0;j<k;j++)
         {
-            d[i][j] = INF;
+            d[i][j] = 1000000000;
         }
     }
     for(int i=0;i<k;i++) cin>>c[i];
@@ -60,30 +48,36 @@ int main ()
         cin>>u>>v>>x;
         g[u].push_back(make_pair(v,x));
         g[v].push_back(make_pair(u,x));
+        d[mp[u]][mp[v]] = min(d[mp[u]][mp[v]],x);
+        d[mp[v]][mp[u]] = min(d[mp[v]][mp[u]],x);
     }
-    
     for(int i=0;i<k;i++)
     {
         for(int j=1;j<=n;j++)
         {
-            dist[j] = INF;
             vis[j] = false;
         }
-        shortestPath(s[i]);
-        for(int j=1;j<=n;j++)
-        {   
-            
-            if(j==s[i] && c[i]!=1) {continue;}
-            d[i][mp[j]] = min(d[i][mp[j]],dist[j]);
-        }
-        
+        my_dfs(s[i]);
+        for(int j=s[i]+1;j<s[i]+c[i];j++)
+        {
+            if(!vis[j])
+                {
+                    cout<<"No";
+                    return 0;
+                }
+        }        
     }
-    
     for(int i=0;i<k;i++)
     {
-        if(d[i][i]!=0) 
+        for(int j=0;j<k;j++)
         {
-            cout<<"No";return 0;
+            for(int r=0;r<k;r++)
+            {
+                if(d[j][r] > d[j][i] + d[i][r])
+                {
+                    d[j][r] = d[j][i] + d[i][r];
+                }
+            }
         }
     }
     cout<<"Yes\n";
@@ -91,7 +85,9 @@ int main ()
     {
         for(int j=0;j<k;j++)
         {
-            if(d[i][j]==INF)
+            if(i==j)
+                cout<<"0 ";
+            else if(d[i][j]==1000000000)
                 cout<<"-1 ";
             else
                 cout<<d[i][j]<<" ";
