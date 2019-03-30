587acda5844cb4b81f61d39bc17c5d9d460652fa
jainrishabh98 <jrishabh98@gmail.com>
2019-03-31 01:47:38 +0530

    Done

--- cf1143D.cpp
+++ cf1143D.cpp
@@ -8,18 +8,12 @@ int main()
     set<ll> s;
     ll t = abs(a - b);
     ll tt = abs(k - a - b);
-    ll ttt = a + b;
-    ll r = k - a + b;
     for (ll i = 0; i <= n; i++)
     {
         if (t + k * i <= n * k)
             s.insert(t + k * i);
         if (tt + k * i <= n * k)
             s.insert(tt + k * i);
-        if (ttt + k * i <= n * k)
-            s.insert(ttt + k * i);
-        if (r + k * i <= n * k)
-            s.insert(r + k * i);
     }
     unsigned long long int mn = n * k, mx = 0,temp;
     for (auto i : s)
@@ -28,9 +22,8 @@ int main()
         {
             temp = __gcd(i, n * k);
             temp = (n * k) / temp;
-            temp *= i;
-            mn = min(mn, temp / i);
-            mx = max(mx, temp / i);
+            mn = min(mn, temp);
+            mx = max(mx, temp);
         }
     }
     cout << mn << " " << mx;
