45f1077d15b685b249e00acd7930323c24d019c5
jainrishabh98 <jrishabh98@gmail.com>
2019-03-31 00:27:10 +0530

    To be done

---
+++ cf167B.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n,l,k;
+    cin>>n>>l>>k;
+    double p[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>p[i];
+    }
+    int cap[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>cap[i];
+    }
+    
+}
