6e55d23f662684d0574e1b2331219846f387521c
jainrishabh98 <jrishabh98@gmail.com>
2019-03-31 21:13:50 +0530

    To be done

---
+++ cf1144E.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+    int k;
+    string s,t;
+    cin>>s>>t;
+    
+}
---
+++ cf1144G.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+map<int, int> mp, mp2;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
