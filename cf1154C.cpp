5b77de317669b12dc0c10416ad09e3a8305ffc6c
jainrishabh98 <jrishabh98@gmail.com>
2019-05-10 23:55:19 +0530

    To be done

---
+++ cf1154C.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    int weeks = min(a/3,min(b/2,c/2));
+    
+}
--- cf1154G.cpp
+++ cf1154G.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 int main ()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
     int n;
     cin>>n;
-    int p[n];
+    int a[n];
     for(int i=0;i<n;i++)
     {
-        cin>>p[i];
+        cin>>a[i];
     }
-
+    
 }
